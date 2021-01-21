d885746218abe623f7635bdf4f3651a2836a38d7
Aleksey Belchenkov <u608110@gmail.com>
2021-01-21 17:03:17 +0300

    Get Video From Camera

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// Global Vars
+let width = 500,
+    height = 0,
+    filter = 'none',
+    streaming = false;
+
+// DOM Elements
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const photos = document.getElementById("photos");
+const photoButton = document.getElementById("photoButton");
+const clearButton = document.getElementById("clearButton");
+const photoFIlter = document.getElementById("photoFIlter");
+
+// Get media stream
+navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(stream => {
+        // Link to the video source
+        video.srcObject = stream;
+        console.log(video);
+        // Play Video
+        video.play();
+    })
+    .catch(err => {
+        console.error(`Error: ${err}`);
+    });
